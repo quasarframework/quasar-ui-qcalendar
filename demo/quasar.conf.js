e937a83ed6311bcc96a297ade296a6b10d5ca763
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-05-20 14:31:21 -0600

    test: YAGT (yet another Github test)

--- demo/quasar.conf.js
+++ demo/quasar.conf.js
@@ -91,7 +91,7 @@ module.exports = function (ctx) {
     build: {
       scopeHoisting: true,
       vueRouterMode: 'history',
-      publicPath: 'https://quasarframework.github.io/app-extension-qcalendar/',
+      publicPath: 'app-extension-qcalendar',
       // vueCompiler: true,
       // gzip: true,
       // analyze: true,
