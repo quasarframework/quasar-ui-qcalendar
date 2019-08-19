f550b739da4bec2916444c67dffab7d29ac67cee
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-19 17:21:53 -0600

    feat(docs): now using qpublish

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -1,6 +1,20 @@
 <template>
   <hero>
     <q-markdown :src="markdown" toc @data="onToc" />
+    <component-api
+      title="QCalendar API"
+      :json="json"
+    />
+    <q-markdown>
+# Donate
+If you appreciate the work that went into this App Extension, please consider [donating to Quasar](https://donate.quasar.dev).
+
+---
+This page created with [QMarkdown](https://quasarframework.github.io/app-extension-qmarkdown), another great Quasar App Extension.
+    </q-markdown>
+    <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+      <q-btn fab icon="keyboard_arrow_up" color="primary" />
+    </q-page-scroller>
   </hero>
 </template>
 
@@ -10,6 +24,7 @@
 <script>
 import Hero from '../components/Hero'
 import markdown from '../markdown/calendar.md'
+import json from '@quasar/quasar-app-extension-qcalendar/src/component/QCalendar.json'
 
 export default {
   name: 'PageIndex',
@@ -20,7 +35,8 @@ export default {
 
   data () {
     return {
-      markdown: markdown
+      markdown: markdown,
+      json: json
     }
   },
 
@@ -39,6 +55,10 @@ export default {
 
   methods: {
     onToc (toc) {
+      // add the manual ones
+      toc.push({ id: 'QCalendar-API', label: 'QCalendar API', level: 1, children: Array(0) })
+      toc.push({ id: 'Donate', label: 'Donate', level: 1, children: Array(0) })
+
       this.toc = toc
     }
   }
