41740f77b13420249215cfec441f7d7aabc98aa8
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-09-04 18:11:57 -0600

    fix: colorize issues

--- src/boot/qcalendar.js
+++ src/boot/qcalendar.js
@@ -1,7 +1,5 @@
 import QCalendar from '@quasar/quasar-app-extension-qcalendar/src/component/QCalendar'
-import { Colorize } from 'quasar-mixin-colorize'
 
 export default ({ Vue }) => {
   Vue.component('q-calendar', QCalendar)
-  Vue.component(Colorize)
 }
