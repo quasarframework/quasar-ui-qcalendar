9ac07d27c63ec16238c6583c61e457860af9fb85
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-11-26 08:30:07 -0700

    fix(ui): dark mode improvements

--- demo/src/components/EssentialLinks.vue
+++ demo/src/components/EssentialLinks.vue
@@ -30,7 +30,7 @@
     <q-separator />
     <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/quasar-ui-qcalendar">
       <q-item-section avatar>
-        <q-icon name="extension" />
+        <q-icon name="home" />
       </q-item-section>
       <q-item-section>
         <q-item-label>QCalendar home</q-item-label>
@@ -39,7 +39,7 @@
     </q-item>
     <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/quasar-ui-qmarkdown">
       <q-item-section avatar>
-        <q-icon name="extension" />
+        <q-icon name="fab fa-markdown" />
       </q-item-section>
       <q-item-section>
         <q-item-label>QMarkdown home</q-item-label>
