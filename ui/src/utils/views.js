3475311c00544e3c4e215b08f90caccb3fe5fe77
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-11-28 17:33:50 -0700

    feat(ui): Agenda view (#58)

--- ui/src/utils/views.js
+++ ui/src/utils/views.js
@@ -3,5 +3,8 @@ export function validateView (view) {
     'month', 'week', 'day',
     '2day', '3day', '4day', '5day', '6day',
     'custom-interval', 'month-interval',
-    'scheduler', 'week-scheduler', 'month-scheduler'].includes(view)
+    'scheduler', 'week-scheduler', 'month-scheduler',
+    'day-agenda', '2day-agenda', '3day-agenda',
+    '4day-agenda', '5day-agenda', '6day-agenda',
+    'week-agenda'].includes(view)
 }
