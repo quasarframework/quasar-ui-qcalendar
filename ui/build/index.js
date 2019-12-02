c87e3c44f5bc23d369a5127db25bad210c22a274
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-02 12:52:28 -0700

    feat(build): sync ui version with app-extension

--- ui/build/index.js
+++ ui/build/index.js
@@ -8,6 +8,7 @@ const { green, blue } = require('chalk')
 
 console.log()
 
+require('./script.app-ext').syncAppExt()
 require('./script.clean.js')
 
 console.log(` 📦 Building ${green('v' + require('../package.json').version)}...${parallel ? blue(' [multi-threaded]') : ''}\n`)
