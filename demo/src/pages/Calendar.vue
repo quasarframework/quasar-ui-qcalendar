c05519191aba1834f4c053dfbe2d50730f8d0d44
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-13 12:16:53 -0700

    chore(demo): adjust day-padding

--- demo/src/pages/Calendar.vue
+++ demo/src/pages/Calendar.vue
@@ -296,7 +296,7 @@
         @click:week="addEventMenu"
         @click:resource="resourceClicked"
         @click:resource:day="resourceDayClicked"
-        dayPadding="35px 2px"
+        day-padding="35px 0 0 0"
       >
         <template #day="{ date }">
           <template v-if="calendarView.indexOf('agenda') < 0">
