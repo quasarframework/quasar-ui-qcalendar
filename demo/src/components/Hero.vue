a786d59cbf55ad3bc3ab5d59ce5fe50302fcee01
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-15 06:27:03 -0700

    chore(demo): improvements to hero banner

--- demo/src/components/Hero.vue
+++ demo/src/components/Hero.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <section class="page-header">
+      <div class="donate"><a :href="donateUrl" target="_blank" title="Donate"><i class="fas fa-heart" style="color: red"></i></a></div>
       <div class="text-h1 project-name">{{ title }}</div>
       <div class="text-h2 project-tagline">{{ tagline }}</div>
-      <div class="byline">{{ byline }}<a :href="bylineTwitter" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></div>
-      <div class="quasar">{{ quasarDesc }}</div>
-      <div class="donate"><a :href="donateUrl" target="_blank" title="Donate"><i class="fas fa-heart" style="color: red"></i></a></div>
       <q-btn type="a" :href="locationUrl" target="_blank" class="btn" label="View on GitHub" no-caps flat/>
       <q-btn to="/docs" class="btn" label="Docs" no-caps flat/>
       <q-btn to="/examples" class="btn" label="Examples" no-caps flat/>
       <q-btn to="/demo" class="btn" label="Interactive Demo" no-caps flat/>
       <q-btn type="a" :href="donateUrl" target="_blank" class="btn" label="Donate" no-caps flat/>
+      <div class="row justify-evenly items-center q-ma-sm">
+        <div class="quasar">{{ quasarDesc }}</div>
+        <div class="byline">{{ byline }}<a :href="bylineTwitter" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></div>
+      </div>
     </section>
     <main class="flex flex-start justify-center inset-shadow">
       <div class="q-pa-md col-12-sm col-8-md col-6-lg inset-shadow" style="width: 100%; height: 3px;" />
