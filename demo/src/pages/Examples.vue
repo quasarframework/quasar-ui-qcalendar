cbeda17fc5135c7ceaf18d47cb755adbcb77b315
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-31 12:00:05 -0700

    chore(demo): code clean up

--- demo/src/pages/Examples.vue
+++ demo/src/pages/Examples.vue
@@ -1,20 +1,17 @@
 <template>
   <hero>
     <router-view />
-    <!-- <old-examples /> -->
   </hero>
 </template>
 
 <script>
 import Hero from '../components/Hero'
-// import OldExamples from './Examples-old'
 
 export default {
   name: 'ExamplesPage',
 
   components: {
     Hero
-    // OldExamples
   }
 }
 </script>
