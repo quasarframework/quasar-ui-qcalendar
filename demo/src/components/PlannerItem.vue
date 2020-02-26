a97a657c67f05833add3a8ead05c3decd5de858a
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-02-26 08:04:10 -0700

    chore(docs): update demo (Planner)

--- demo/src/components/PlannerItem.vue
+++ demo/src/components/PlannerItem.vue
@@ -1,53 +1,62 @@
 <template>
-  <q-card class="q-mr-xs q-mb-xs q-px-sm">
-    <div class="row items-center justify-start">
-      <div class="col" style="max-width: 30px; width: 100%">
+  <q-card class="q-mr-xs q-mb-xs q-px-sm planner-item">
+
+    <div class="row items-center justify-start no-wrap">
+      <div class="col" style="max-width: 25px; min-width: 25px;">
         <q-icon :name="value ? 'check_box' : 'check_box_outline_blank'" :class="'cursor-pointer' + (value ? ' text-red-8' : ' text-blue-8')" @click="$emit('input', !value)" />
       </div>
-      <div :class="'ellipsis col-grow' + (value ? ' text-red-8' : ' text-blue-8')">{{ name }}</div>
+      <div :class="'ellipsis col' + (value ? ' text-red-8' : ' text-blue-8')">{{ name }}</div>
     </div>
-    <div class="row items-center justify-start">
-      <div class="col" style="max-width: 30px; width: 100%">
+
+    <div class="row items-center justify-start no-wrap">
+      <div class="col" style="max-width: 25px; min-width: 25px;">
         <q-icon name="location_on" />
       </div>
-      <div class="ellipsis col-grow">{{ address }}</div>
+      <div class="ellipsis col">{{ address }}</div>
     </div>
-    <div class="row items-center justify-start">
-      <div class="col" style="max-width: 30px; width: 100%">
+
+    <div class="row items-center justify-start no-wrap">
+      <div class="col" style="max-width: 25px; min-width: 25px;">
         <q-icon name="mail_outline" />
       </div>
-      <div class="ellipsis col-grow">{{ email }}</div>
+      <div class="ellipsis col">{{ email }}</div>
     </div>
-    <div class="row items-center justify-start">
-      <div class="col" style="max-width: 30px; width: 100%">
+
+    <div class="row items-center justify-start no-wrap">
+      <div class="col" style="max-width: 25px; min-width: 25px;">
         <q-icon name="call" />
       </div>
-      <div class="ellipsis col-grow">{{ phone }}</div>
+      <div class="ellipsis col">{{ phone }}</div>
     </div>
-    <div class="row items-center justify-start">
-      <div class="col" style="max-width: 30px; width: 100%">
+
+    <div class="row items-center justify-start no-wrap">
+      <div class="col" style="max-width: 25px; min-width: 25px;">
         <q-icon name="work" />
       </div>
-      <div class="ellipsis col-grow">{{ workDone }}</div>
+      <div class="ellipsis col">{{ workDone }}</div>
     </div>
-    <div class="row items-center justify-start">
-      <div class="col" style="max-width: 30px; width: 100%">
+
+    <div class="row items-center justify-start no-wrap">
+      <div class="col" style="max-width: 25px; min-width: 25px;">
         <q-icon name="calendar_today" />
       </div>
-      <div class="ellipsis col-grow">{{ workDate }}</div>
+      <div class="ellipsis col">{{ workDate }}</div>
     </div>
-    <div class="row items-center justify-start">
-      <div class="col" style="max-width: 30px; width: 100%">
+
+    <div class="row items-center justify-start no-wrap">
+      <div class="col" style="max-width: 25px; min-width: 25px;">
         <q-icon name="attach_money" />
       </div>
-      <div class="ellipsis col-grow">{{ amount }}</div>
+      <div class="ellipsis col">{{ amount }}</div>
     </div>
-    <div v-if="daysOver" class="row items-center justify-start">
-      <div class="col text-red-8" style="max-width: 30px; width: 100%">
+
+    <div v-if="daysOver" class="row items-center justify-start no-wrap">
+      <div class="col text-red-8" style="max-width: 25px; min-width: 25px;">
         <q-icon name="access_alarm" />
       </div>
-      <div class="ellipsis col-grow text-red-8">{{ daysOver }} days overdue</div>
+      <div class="ellipsis col text-red-8">{{ daysOver }} days overdue</div>
     </div>
+
   </q-card>
 </template>
 
@@ -72,3 +81,11 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.planner-item
+  border: 1px solid transparent
+  &:hover
+    border: 1px solid $blue-8
+    cursor: pointer
+</style>
