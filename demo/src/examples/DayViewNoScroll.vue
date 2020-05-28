79e249e8682e982bbe2cea64b31eee0cf9099829
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-05-28 13:40:10 -0600

    chore: update examples

--- demo/src/examples/DayViewNoScroll.vue
+++ demo/src/examples/DayViewNoScroll.vue
@@ -2,13 +2,13 @@
   <div class="row justify-center q-pa-lg">
     <q-scroll-area
       style="height: 400px; max-width: 600px; width: 100%;"
+      class="content-bordered"
     >
       <q-calendar
         v-model="selectedDate"
         view="day"
         locale="en-us"
         no-scroll
-        bordered
       />
     </q-scroll-area>
   </div>
@@ -18,8 +18,13 @@
 export default {
   data () {
     return {
-      selectedDate: '2019-04-01'
+      selectedDate: ''
     }
   }
 }
 </script>
+
+<style lang="sass">
+.content-bordered
+  border: 1px solid #e0e0e0
+</style>
--- ui/dev/src/pages/DayViewNoScroll.vue
+++ ui/dev/src/pages/DayViewNoScroll.vue
@@ -2,13 +2,13 @@
   <div class="row justify-center q-pa-lg">
     <q-scroll-area
       style="height: 400px; max-width: 600px; width: 100%;"
+      class="content-bordered"
     >
       <q-calendar
         v-model="selectedDate"
         view="day"
         locale="en-us"
         no-scroll
-        bordered
       />
     </q-scroll-area>
   </div>
@@ -23,3 +23,8 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+.content-bordered
+  border: 1px solid #e0e0e0
+</style>
