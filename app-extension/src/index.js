eae2c78f6b6a3f29dd868548f3b91ce3bee9c513
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-06-19 07:52:56 -0600

    fix: AE compatibility with q/app v2

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -22,8 +22,8 @@ module.exports = function (api) {
   // Quasar compatibility check; you may need
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app" CLI
-  api.compatibleWith('quasar', '^1.5.0')
-  api.compatibleWith('@quasar/app', '^1.3.0') // needed for describe to work properly
+  // api.compatibleWith('quasar', '^1.5.0')
+  api.compatibleWith('@quasar/app', '^1.3.0 || ^2.0.0') // needed for describe to work properly
 
   // Uncomment the line below if you provide a JSON API for your component
   api.registerDescribeApi('QCalendar', '~@quasar/quasar-ui-qcalendar/dist/api/QCalendar.json')
