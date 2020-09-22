5d3ae1f5042de28392a814a651ec9a48f830a2a7
Alexandre Martins <a.martins@auranext.com>
2020-09-22 17:11:42 +0100

    Improved DnD demo page

--- ui/dev/src/pages/ResouceViewBasicDnD.vue
+++ ui/dev/src/pages/ResouceViewBasicDnD.vue
@@ -22,14 +22,16 @@
       style="height: 400px; max-width: 1300px; width: 100%;"
     >
       <template #resource-intervals="scope">
-        <template v-for="(event, index) in events.filter(e => e.date === scope.intervals[0][0].date && e.resourceId == scope.resource.resourceId)">
+        <template v-for="(schedule, index) in events.filter(e => e.date === scope.intervals[0][0].date && e.resourceId == scope.resource.resourceId)">
           <q-badge
             :key="index"
             draggable
+            @dragstart.stop="(event) => onDragStart(event, schedule)"
+            @mousemove.stop="onMouseMove"
             class="q-pa-sm"
-            :style="`position: absolute; left: ${scope.timeStartPosX(event.start)}px; color: ${event.txtColor}; background-color: ${event.bgColor}`"
+            :style="`position: absolute; left: ${scope.timeStartPosX(schedule.start)}px; color: ${schedule.txtColor}; background-color: ${schedule.bgColor}; width: ${eventWidth(schedule, scope.timeDurationWidth)}`"
           >
-            {{event.title}}
+            {{schedule.title}}
           </q-badge>
         </template>
       </template>
@@ -42,6 +44,7 @@ export default {
   data () {
     return {
       selectedDate: '2020-09-21',
+      movingCopy: null,
       resources: [
         { label: 'John', resourceId: 0 },
         { label: 'Mary', resourceId: 1 },
@@ -54,6 +57,7 @@ export default {
       ],
       events: [
         {
+          eventId: '0',
           resourceId: 0,
           date: '2020-09-21',
           start: '08:30',
@@ -63,6 +67,7 @@ export default {
           txtColor: '#fff'
         },
         {
+          eventId: '1',
           resourceId: 2,
           date: '2020-09-21',
           start: '08:30',
@@ -72,6 +77,7 @@ export default {
           txtColor: '#fff'
         },
         {
+          eventId: '2',
           resourceId: 3,
           date: '2020-09-21',
           start: '08:30',
@@ -85,14 +91,57 @@ export default {
   },
 
   methods: {
-    onDrop (event, resource, type, index) {
+    getEventDurationMin (event) {
+      const start = new Date(event.date + ' ' + event.start + '+0')
+      const end = new Date(event.date + ' ' + event.end + '+0')
+      const ms = end.getTime() - start.getTime()
+      const mins = ms / (60 * 1000) // 1 min = 60s = (60 * 1000)ms
+      return mins
+    },
+
+    onMouseMove (evt) {
+      // console.log(`Mouse: (${evt.offsetX}, ${evt.offsetY}) => inside badge (${evt.target.offsetLeft + evt.offsetX}, ${evt.target.offsetTop + evt.offsetY})`)
+    },
+
+    eventWidth (event, timeDurationWidth) {
+      return timeDurationWidth(this.getEventDurationMin(event)) + 'px'
+    },
+
+    onDragStart (event, schedule) {
+      console.log(event.source)
+
+      event.dataTransfer.setData('text/json', JSON.stringify(schedule))
+      event.effectAllowed = 'copyMove'
+      event.dataTransfer.setDragImage(event.target, 0, 0);
+      // set badge position start to 
+    },
+
+
+    onDrop (event, resource, type, interval) {
       console.log('Drop')
       console.log(event);
       console.log(resource)
       console.log(type)
-      console.log(index)
-      alert(`Drop on ${index.time}`)
+      console.log(interval)
+      const schedule = JSON.parse(event.dataTransfer.getData('text/json'))
+      // calculate new end time
+      const time = new Date(`${interval.date} ${interval.time}`)
+      const durMins = this.getEventDurationMin(schedule)
+      const timestamp = time.getTime() + durMins * 60 * 1000
+      const newDate = new Date(timestamp)
+      const newEndTime = `${newDate.getHours()}:${newDate.getMinutes()}`
+
+      const sch2change = this.events.filter(e => e.eventId === schedule.eventId)[0]
+      sch2change.start = interval.time
+      sch2change.end = newEndTime
+      sch2change.resourceId = resource.resourceId
+      alert(`Drop on ${interval.time} => end at ${newEndTime}`)
+
+      // if (this.movingCopy !== undefined) {
+      //   document.body.removeChild(this.movingCopy)
+      // }
     },
+
     onDragOver (e, day, type) {
       console.log('Drag over')
       e.preventDefault()
@@ -102,6 +151,7 @@ export default {
       if (type === 'interval') {
         console.log(day)
       }
+
     },
   }
 }
