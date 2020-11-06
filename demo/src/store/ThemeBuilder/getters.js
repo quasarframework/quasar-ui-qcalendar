cd9f7afb517cd30cd6df1aeb4e42ef44211b31ae
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-11-06 09:35:49 -0700

    chore(demo): add hints for ThemeEditor

--- demo/src/store/ThemeBuilder/getters.js
+++ demo/src/store/ThemeBuilder/getters.js
@@ -1,2 +1,3 @@
 
 export const style = (state) => state.style
+export const hints = (state) => state.hints
--- demo/src/store/ThemeBuilder/state.js
+++ demo/src/store/ThemeBuilder/state.js
@@ -80,6 +80,87 @@ export default function () {
       '--calendar-mini-work-week-width': '30',
       '--calendar-work-week-font-size': '1.0em',
       '--calendar-head-font-weight': '600'
+    },
+    hints: {
+      '--calendar-border': 'The regular borders',
+      '--calendar-border-dark': '[DARK] The regular borders',
+      '--calendar-border-section': 'Interval borders (borders between regular borders for interval-based calendars)',
+      '--calendar-border-section-dark': '[DARK] Interval borders (borders between regular borders for interval-based calendars)',
+
+      '--calendar-border-current': 'The border used for `current day`',
+      '--calendar-border-current-dark': '[DARK] The border used for `current day`',
+      '--calendar-border-droppable': 'The border to be used for a `drop event`',
+      '--calendar-border-droppable-dark': '[DARK] The border to be used for a `drop event`',
+
+      '--calendar-mini-selected-day-connector-hover-border': 'The border to use beween the two selection days when `hover` property is used. Applicable for `month` calendar with property `mini-mode`',
+      '--calendar-mini-selected-day-connector-hover-border-dark': '[DARK] The border to use beween the two selection days when `hover` property is used. Applicable for `month` calendar with property `mini-mode`',
+
+      '--calendar-color': 'The text color of the calendar',
+      '--calendar-color-dark': '[DARK] The text color of the calendar',
+      '--calendar-background': 'The background of the calendar',
+      '--calendar-background-dark': '[DARK] The background of the calendar',
+
+      '--calendar-current-color': 'The text color of the `current` date',
+      '--calendar-current-color-dark': '[DARK] The text color of the `current` date',
+      '--calendar-current-background': 'The background color of the `current` date',
+      '--calendar-current-background-dark': '[DARK] The background color of the `current` date',
+
+      '--calendar-active-date-color': 'The text color of the `active` date',
+      '--calendar-active-date-color-dark': '[DARK] The text color of the `active` date',
+      '--calendar-active-date-background': 'The background color of the `active` date',
+      '--calendar-active-date-background-dark': '[DARK] The background color of the `active` date',
+
+      '--calendar-outside-color': 'The text color of an `outside` date',
+      '--calendar-outside-color-dark': '[DARK] The text color of an `outside` date',
+      '--calendar-outside-background': 'The background color of an `outside` date',
+      '--calendar-outside-background-dark': '[DARK] The background color of an `outside` date',
+
+      '--calendar-selected-date-color': 'The text color of a `selected` **date**',
+      '--calendar-selected-date-color-dark': '[DARK] The text color of a `selected` **date**',
+      '--calendar-selected-date-background': 'The background color of a `selected` **date**',
+      '--calendar-selected-date-background-dark': '[DARK] The background color of a `selected` **date**',
+
+      '--calendar-mini-selected-date-color': 'The text color of a `selected` **date** for `monthly mini-mode`',
+      '--calendar-mini-selected-date-color-dark': '[DARK] The text color of a `selected` **date** for `monthly mini-mode`',
+      '--calendar-mini-selected-date-background': 'The background color of a `selected` **date** for `monthly mini-mode`',
+      '--calendar-mini-selected-date-background-dark': '[DARK] The background color of a `selected` **date** for `monthly mini-mode`',
+      '--calendar-mini-selected-date-label-color': 'The label text color of a `selected` **date** for `monthly mini-mode`',
+      '--calendar-mini-selected-date-label-color-dark': '[DARK] The label text color of a `selected` **date** for `monthly mini-mode`',
+      '--calendar-mini-selected-date-label-background': 'The label background color of a `selected` **date** for `monthly mini-mode`',
+      '--calendar-mini-selected-date-label-background-dark': '[DARK] The label background color of a `selected` **date** for `monthly mini-mode`',
+
+      '--calendar-selected-day-color': 'The text color of a `selected` **day**',
+      '--calendar-selected-day-color-dark': '[DARK] The text color of a `selected` **day**',
+      '--calendar-selected-day-background': 'The background color of a `selected` **day**',
+      '--calendar-selected-day-background-dark': '[DARK] The background color of a `selected` **day**',
+
+      '--calendar-mini-selected-day-color': 'The text color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-color-dark': '[DARK] The text color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-background': 'The background color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-background-dark': '[DARK] The background color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-label-color': 'The label text color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-label-color-dark': '[DARK] The label text color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-label-background': 'The label background color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-label-background-dark': '[DARK] The label background color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-connector-color': 'The connector color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-connector-color-dark': '[DARK] The connector color of a `selected` **day** for `monthly mini-mode`',
+      '--calendar-mini-selected-day-hover-color': 'The hover color of a `selected` **day** for `monthly mini-mode` when `hover` property is set',
+      '--calendar-mini-selected-day-hover-color-dark': '[DARK] The hover color of a `selected` **day** for `monthly mini-mode` when `hover` property is set',
+
+      '--calendar-mini-selected-day-firstlast-color': 'The text color of the first and last selected days for `monthly mini-mode`',
+      '--calendar-mini-selected-day-firstlast-color-dark': '[DARK] The text color of the first and last selected days for `monthly mini-mode`',
+      '--calendar-mini-selected-day-firstlast-background': 'The background color of the first and last selected days for `monthly mini-mode`',
+      '--calendar-mini-selected-day-firstlast-background-dark': '[DARK] The background color of the first and last selected days for `monthly mini-mode`',
+      '--calendar-mini-selected-day-firstlast-label-color': 'The label text color of the first and last selected days for `monthly mini-mode`',
+      '--calendar-mini-selected-day-firstlast-label-color-dark': '[DARK] The label text color of the first and last selected days for `monthly mini-mode`',
+      '--calendar-mini-selected-day-firstlast-label-background': 'The label background color of the first and last selected days for `monthly mini-mode`',
+      '--calendar-mini-selected-day-firstlast-label-background-dark': '[DARK] The label background color of the first and last selected days for `monthly mini-mode`',
+
+      '--calendar-intervals-width': 'The intervals width',
+      '--calendar-work-week-width': 'The work week width',
+      '--calendar-mini-work-week-width': 'The work week width for `month mini-mode`',
+      '--calendar-work-week-font-size': 'The work week font size',
+      '--calendar-head-font-weight': 'The header font weight'
     }
   }
 }
