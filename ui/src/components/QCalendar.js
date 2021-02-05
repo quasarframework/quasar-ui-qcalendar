4b57bc954aa64cbf789c0a3fa809f4ac19383fdb
Jeff Galbraith <jeff.galbraith@xendra.com>
2021-02-05 08:26:11 -0700

    chore(ui): code clean up

--- ui/src/components/QCalendar.js
+++ ui/src/components/QCalendar.js
@@ -2,6 +2,13 @@ import {
   defineComponent
 } from 'vue'
 
+// import QCalendarAgenda from './components/QCalendarAgenda.js'
+// import QCalendarDay from './components/QCalendarDay.js'
+// import QCalendarGrid from './components/QCalendarGrid.js'
+// import QCalendarMonth from './components/QCalendarMonth.js'
+// import QCalendarResource from './components/QCalendarResource.js'
+// import QCalendarScheduler from './components/QCalendarScheduler.js'
+
 export default defineComponent({
   name: 'QCalendar'
 })
