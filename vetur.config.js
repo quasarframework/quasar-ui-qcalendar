578e745a177250c0eb1aa0da35dce0548b7d7fe7
Jeff Galbraith <jeff.galbraith@xendra.com>
2021-02-05 08:25:35 -0700

    chore: add vetur interpolation

---
+++ vetur.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fg = require('fast-glob')
+const projects = fg.sync('./*', {
+  onlyDirectories: true,
+  cwd: __dirname
+})
+
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  // **optional** default: `{}`
+  // override vscode settings
+  // Notice: It only affects the settings used by Vetur.
+  settings: {
+    'vetur.useWorkspaceDependencies': true,
+    'vetur.validation.template': false,
+    'vetur.experimental.templateInterpolationService': true,
+  },
+  // support monorepos
+  projects
+}
