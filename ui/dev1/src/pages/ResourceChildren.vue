afd189c2e3d4eb5b2fdd168dae7d7a29e9659a8d
Jeff Galbraith <jeff.galbraith@xendra.com>
2021-03-13 08:00:29 -0700

    chore(dev): updated examples

---
+++ ui/dev1/src/pages/ResourceChildren.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="subcontent">
+    <navigation-bar
+      @today="onToday"
+      @prev="onPrev"
+      @next="onNext"
+    />
+
+    <div style="display: flex; justify-content: center;">
+      <QCalendarResource
+        ref="calendar"
+        v-model="selectedDate"
+        v-model:modelResources="resources"
+        resource-key="id"
+        resource-label="name"
+        bordered
+        sticky
+        style="max-width: 800px; width: 100%; max-height: 400px;"
+        @change="onChange"
+        @moved="onMoved"
+        @resource-expanded="onResourceExpanded"
+        @click-date="onClickDate"
+        @click-time="onClickTime"
+        @click-resource="onClickResource"
+        @click-head-resources="onClickHeadResources"
+        @click-interval="onClickInterval"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { QCalendarResource } from '@quasar/quasar-ui-qcalendar/QCalendarResource.js'
+import { today } from '@quasar/quasar-ui-qcalendar/Timestamp.js'
+import '@quasar/quasar-ui-qcalendar/QCalendarVariables.sass'
+import '@quasar/quasar-ui-qcalendar/QCalendarTransitions.sass'
+import '@quasar/quasar-ui-qcalendar/QCalendarResource.sass'
+
+import { defineComponent } from 'vue'
+import NavigationBar from '../components/NavigationBar.vue'
+
+export default defineComponent({
+  name: 'ResourceDark',
+  components: {
+    NavigationBar,
+    QCalendarResource
+  },
+  data () {
+    return {
+      selectedDate: today(),
+      resources: [
+        { id: '1', name: 'John' },
+        {
+          id: '2',
+          name: 'Board Room',
+          expanded: false,
+          children: [
+            { id: '2.1', name: 'Room-1' },
+            {
+              id: '2.2',
+              name: 'Room-2',
+              expanded: false,
+              children: [
+                { id: '2.2.1', name: 'Partition-A' },
+                { id: '2.2.2', name: 'Partition-B' },
+                { id: '2.2.2', name: 'Partition-C' }
+              ]
+            }
+          ]
+        },
+        { id: '3', name: 'Mary' },
+        { id: '4', name: 'Susan' },
+        { id: '5', name: 'Olivia' }
+      ]
+    }
+  },
+  methods: {
+    onToday () {
+      this.$refs.calendar.moveToToday()
+    },
+    onPrev () {
+      this.$refs.calendar.prev()
+    },
+    onNext () {
+      this.$refs.calendar.next()
+    },
+    onMoved (data) {
+      console.log('onMoved', data)
+    },
+    onChange (data) {
+      console.log('onChange', data)
+    },
+    onResourceExpanded (data) {
+      console.log('onResourceExpanded', data)
+    },
+    onClickDate (data) {
+      console.log('onClickDate', data)
+    },
+    onClickTime (data) {
+      console.log('onClickTime', data)
+    },
+    onClickResource (data) {
+      console.log('onClickResource', data)
+    },
+    onClickHeadResources (data) {
+      console.log('onClickHeadResources', data)
+    },
+    onClickInterval (data) {
+      console.log('onClickInterval', data)
+    }
+  }
+})
+</script>
