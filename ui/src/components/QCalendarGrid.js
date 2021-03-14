13f0e293131e8139d27a2b4931f24f8d61d36a8f
Jeff Galbraith <jeff.galbraith@xendra.com>
2021-03-14 13:45:39 -0600

    feat(ui): new calendar component QCalendarGrid [wip]

--- ui/src/components/QCalendarGrid.js
+++ ui/src/components/QCalendarGrid.js
@@ -1,7 +1,643 @@
 import {
-  defineComponent
+  h,
+  computed,
+  defineComponent,
+  getCurrentInstance,
+  onBeforeUpdate,
+  onMounted,
+  // nextTick,
+  reactive,
+  ref,
+  Transition,
+  watch,
+  withDirectives
 } from 'vue'
 
+// Utility
+import {
+  getDayIdentifier,
+  parsed,
+  parseTimestamp,
+  today
+} from '../utils/Timestamp.js'
+
+import {
+  convertToUnit,
+  minCharWidth
+} from '../utils/helpers.js'
+
+import useMouse, { getRawMouseEvents } from '../composables/useMouse.js'
+
+import useCalendar from '../composables/useCalendar.js'
+import useCommon, { useCommonProps } from '../composables/useCommon.js'
+import useGrid, { useGridProps } from '../composables/useGrid.js'
+import useTimes, { useTimesProps } from '../composables/useTimes.js'
+import useRenderValues from '../composables/useRenderValues.js'
+import useMove, { useMoveEmits } from '../composables/useMove.js'
+import useEmitListeners from '../composables/useEmitListeners.js'
+import useButton from '../composables/useButton.js'
+import useFocusHelper from '../composables/useFocusHelper.js'
+import useCellWidth, { useCellWidthProps } from '../composables/useCellWidth.js'
+import useCheckChange, { useCheckChangeEmits } from '../composables/useCheckChange.js'
+import useEvents from '../composables/useEvents.js'
+import useKeyboard, { useNavigationProps } from '../composables/useKeyboard.js'
+
+// Directives
+import ResizeObserver from '../directives/ResizeObserver.js'
+
 export default defineComponent({
-  name: 'QCalendarGrid'
+  name: 'QCalendarGrid',
+
+  directives: [ResizeObserver],
+
+  props: {
+    ...useGridProps,
+    ...useTimesProps,
+    ...useNavigationProps
+  },
+
+  emits: [
+    'update:modelValue',
+    ...useCheckChangeEmits,
+    ...useMoveEmits
+    // ...getRawMouseEvents('-date'),
+    // ...getRawMouseEvents('-day'),
+    // ...getRawMouseEvents('-head-day')
+  ],
+
+  setup (props, { slots, emit, expose }) {
+    const
+      scrollArea = ref(null),
+      pane = ref(null),
+      direction = ref('next'),
+      startDate = ref(props.modelValue || today()),
+      endDate = ref('0000-00-00'),
+      maxDaysRendered = ref(0), // always 0
+      emittedValue = ref(props.modelValue),
+      focusRef = ref(null),
+      focusValue = ref(null),
+      datesRef = ref({}),
+      // taskRef = ref(null),
+      // weekEventRef = ref([]),
+      // weekRef = ref([]),
+      // headerColumnRef = ref(null),
+      size = reactive({ width: 0, height: 0 }),
+      // dragOverDayRef = ref(false),
+      // keep track of last seen start and end dates
+      lastStart = ref(null),
+      lastEnd = ref(null)
+
+    const parsedView = computed(() => {
+      return props.view
+    })
+
+    const vm = getCurrentInstance()
+    if (vm === null) {
+      throw new Error('current instance is null')
+    }
+
+    // initialize emit listeners
+    const { emitListeners } = useEmitListeners(vm)
+
+    const {
+      times,
+      setCurrent,
+      updateCurrent
+    } = useTimes(props)
+
+    // update dates
+    updateCurrent()
+    setCurrent()
+
+    const {
+      // computed
+      weekdaySkips,
+      parsedStart,
+      // parsedEnd,
+      // dayFormatter,
+      weekdayFormatter
+      // ariaDateFormatter,
+      // methods
+      // dayStyleDefault,
+      // getRelativeClasses
+    } = useCommon(props, { startDate, endDate, times })
+
+    const parsedValue = computed(() => {
+      return parseTimestamp(props.modelValue, times.now)
+        || parsedStart.value
+        || times.today
+    })
+
+    focusValue.value = parsedValue.value
+    focusRef.value = parsedValue.value.date
+
+    // const computedStyles = computed(() => {
+    //   const style = {}
+    //   style.minWidth = computedWidth.value
+    //   style.maxWidth = computedWidth.value
+    //   style.width = computedWidth.value
+    //   return style
+    // })
+
+    const { renderValues } = useRenderValues(props, {
+      parsedView,
+      times,
+      parsedValue
+    })
+
+    const {
+      rootRef,
+      __initCalendar,
+      __renderCalendar
+    } = useCalendar(props, __renderGrid, {
+      scrollArea,
+      pane
+    })
+
+    const {
+      // computed
+      parsedStartDate,
+      parsedEndDate,
+      days
+      // methods
+    } = useGrid(props, emit, {
+      weekdaySkips,
+      times
+    })
+
+    const { move } = useMove(props, {
+      parsedView,
+      parsedValue,
+      weekdaySkips,
+      direction,
+      maxDays: maxDaysRendered,
+      times,
+      emittedValue,
+      emit
+    })
+
+    const {
+      getDefaultMouseEventHandlers
+    } = useMouse(emit, emitListeners)
+
+    const {
+      checkChange
+    } = useCheckChange(emit, { days, lastStart, lastEnd })
+
+    const {
+      isKeyCode
+    } = useEvents()
+
+    useKeyboard(props, {
+      rootRef,
+      focusRef,
+      focusValue,
+      days,
+      parsedView,
+      parsedValue,
+      emittedValue,
+      weekdaySkips,
+      direction,
+      times
+    })
+
+    const parsedColumnCount = computed(() => {
+      return days.value.length
+    })
+
+    const borderWidth = computed(() => {
+      if (rootRef.value) {
+        const calendarBorderWidth = getComputedStyle(rootRef.value).getPropertyValue('--calendar-border')
+        const parts = calendarBorderWidth.split(' ')
+        const part = parts.filter(part => part.indexOf('px') > -1)
+        return parseInt(part[ 0 ], 0)
+      }
+      return 0
+    })
+
+    const parsedCellWidth = computed(() => {
+      return props.cellWidth[ props.view ]
+    })
+
+    // const computedWidth = computed(() => {
+    //   if (rootRef.value) {
+    //     const width = size.width || rootRef.value.getBoundingClientRect().width
+    //     if (width && borderWidth.value && parsedColumnCount.value) {
+    //       return ((width - (borderWidth.value * parsedColumnCount.value)) / parsedColumnCount.value) + 'px'
+    //     }
+    //   }
+    //   return (100 / parsedColumnCount.value) + '%'
+    // })
+
+    function __isCheckChange () {
+      if (checkChange() === true
+      && props.useNavigation === true
+      && datesRef.value
+      && focusRef.value) {
+        if (document && document.activeElement !== datesRef.value[ focusRef.value ]) {
+          let count = 0
+          const interval = setInterval(() => {
+            if (datesRef.value[ focusRef.value ]) {
+              datesRef.value[ focusRef.value ].focus()
+              if (++count === 10 || document.activeElement === datesRef.value[ focusRef.value ]) {
+                clearInterval(interval)
+              }
+            }
+            else {
+              clearInterval(interval)
+            }
+          }, 250)
+        }
+      }
+    }
+
+    watch([days], __isCheckChange, { deep: true })
+
+    watch(() => props.modelValue, (val, oldVal) => {
+      if (emittedValue.value !== val) {
+        if (props.animated === true) {
+          const v1 = getDayIdentifier(parsed(val))
+          const v2 = getDayIdentifier(parsed(oldVal))
+          direction.value = v1 >= v2 ? 'next' : 'prev'
+        }
+        emittedValue.value = val
+      }
+      focusRef.value = val
+    })
+
+    watch(emittedValue, (val, oldVal) => {
+      if (emittedValue.value !== props.modelValue) {
+        if (props.animated === true) {
+          const v1 = getDayIdentifier(parsed(val))
+          const v2 = getDayIdentifier(parsed(oldVal))
+          direction.value = v1 >= v2 ? 'next' : 'prev'
+        }
+        emit('update:modelValue', val)
+      }
+    })
+
+    watch(focusRef, val => {
+      if (val) {
+        focusValue.value = parseTimestamp(val)
+      }
+    })
+
+    watch(focusValue, (val) => {
+      if (datesRef.value[ focusRef.value ]) {
+        datesRef.value[ focusRef.value ].focus()
+      }
+    })
+
+    onBeforeUpdate(() => {
+      datesRef.value = {}
+      // weekEventRef.value = []
+      // weekRef.value = []
+    })
+
+    onMounted(() => {
+      __initCalendar()
+    })
+
+    // public functions
+
+    function moveToToday () {
+      emittedValue.value = today()
+    }
+
+    function next (amount = 1) {
+      move(amount)
+    }
+
+    function prev (amount = 1) {
+      move(-amount)
+    }
+
+    // private functions
+
+    function __onResize ({ width, height }) {
+      size.width = width
+      size.height = height
+    }
+
+    // function __isActiveDate (day) {
+    //   return day.date === emittedValue.value
+    // }
+
+    function __renderTaskDay (day, task, index) {
+      const slot = slots.day
+      const scope = {
+        day,
+        task,
+        index
+      }
+      const width = convertToUnit(parsedCellWidth.value)
+      const style = {
+        width,
+        minWidth: width,
+        maxWidth: width
+      }
+
+      const dayClass = typeof props.dayClass === 'function' ? props.dayClass({ scope }) : {}
+
+      return h('div', {
+        class: {
+          'q-calendar-grid__task--day': true,
+          ...dayClass
+        },
+        style
+      }, [
+        slot && slot({ scope })
+      ])
+    }
+
+    function __renderTaskDays (task, index) {
+      return days.value.map(day => __renderTaskDay(day, task, index))
+    }
+
+    function __renderTaskDaysWrapper (task, index) {
+      return h('div', {
+        style: {
+          display: 'flex',
+          flex: 'none'
+        }
+      }, [
+        __renderTaskDays(task, index)
+      ])
+    }
+
+    function __renderTaskItem (task, index) {
+      const slot = slots.task
+      const scope = {
+        start: parsedStartDate.value,
+        end: parsedEndDate.value,
+        task,
+        index
+      }
+      const width = convertToUnit(props.taskWidth)
+      const style = {
+        width,
+        minWidth: width,
+        maxWidth: width
+      }
+
+      return h('div', {
+        class: {
+          'q-calendar-grid__task--item': true,
+          'q-calendar__sticky': true
+        },
+        style
+      }, [
+        slot && slot({ scope })
+      ])
+    }
+
+    function __renderTask (task, index) {
+      return h('div', {
+        class: 'q-calendar-grid__task'
+      }, [
+        __renderTaskItem(task, index),
+        __renderTaskDaysWrapper(task, index)
+      ])
+    }
+
+    function __renderTasks () {
+      return props.tasks.map((task, index) => __renderTask(task, index))
+    }
+
+    function __renderFooterTask () {
+      const slot = slots[ 'footer-task' ]
+      const scope = {
+        start: parsedStartDate.value,
+        end: parsedEndDate.value,
+        tasks: props.tasks
+      }
+      const width = convertToUnit(props.taskWidth)
+      const style = {
+        width,
+        minWidth: width,
+        maxWidth: width
+      }
+
+      return h('div', {
+        class: {
+          'q-calendar-grid__footer--task': true,
+          'q-calendar__sticky': true
+        },
+        style
+      }, [
+        slot && slot({ scope })
+      ])
+    }
+
+    function __renderFooterDay (day) {
+      const slot = slots[ 'footer-day' ]
+      const scope = {
+        day,
+        tasks: props.tasks
+      }
+      const width = convertToUnit(parsedCellWidth.value)
+      const style = {
+        width,
+        minWidth: width,
+        maxWidth: width
+      }
+
+      const footerDayClass = typeof props.footerDayClass === 'function' ? props.footerDayClass({ scope }) : {}
+
+      return h('div', {
+        class: {
+          'q-calendar-grid__footer--day': true,
+          ...footerDayClass
+        },
+        style
+      }, [
+        slot && slot({ scope })
+      ])
+    }
+
+    function __renderFooterDays () {
+      return h('div', {
+        class: 'q-calendar-grid__footer--day-wrapper'
+      }, [
+        days.value.map(day => __renderFooterDay(day))
+      ])
+    }
+
+    function __renderFooter () {
+      return h('div', {
+        class: 'q-calendar-grid__footer'
+      }, [
+        __renderFooterTask(),
+        __renderFooterDays()
+      ])
+    }
+
+    function __renderBody () {
+      return h('div', {
+        class: {
+          'q-calendar-grid__body': true
+        }
+      }, [
+        props.noHeader !== true && __renderHead(),
+        __renderTasks(),
+        __renderFooter()
+      ])
+    }
+
+    function __renderHeadTask () {
+      const slot = slots[ 'head-task' ]
+      const scope = {
+        start: parsedStartDate.value,
+        end: parsedEndDate.value
+      }
+      const width = convertToUnit(props.taskWidth)
+      const style = {
+        width,
+        minWidth: width,
+        maxWidth: width
+      }
+
+      return h('div', {
+        class: {
+          'q-calendar-grid__head--task': true,
+          'q-calendar__sticky': true
+        },
+        style
+      }, [
+        slot && slot({ scope })
+      ])
+    }
+
+    function __renderHeadWeekday (day) {
+      const weekdayLabel = minCharWidth(weekdayFormatter.value(day, true), props.minLabelLength)
+      return h('div', {}, weekdayLabel)
+    }
+
+    function __renderHeadDate (day) {
+      return h('div', {}, day.day)
+    }
+
+    function __renderHeadDay (day) {
+      const width = convertToUnit(parsedCellWidth.value)
+      const style = {
+        width,
+        minWidth: width,
+        maxWidth: width
+      }
+      const scope = {
+        timestamp: day
+      }
+
+      const weekdayClass = typeof props.weekdayClass === 'function' ? props.weekdayClass({ scope }) : {}
+
+      return h('div', {
+        class: {
+          'q-calendar-grid__head--day': true,
+          ...weekdayClass
+        },
+        style
+      }, [
+        __renderHeadDate(day),
+        __renderHeadWeekday(day)
+      ])
+    }
+
+    function __renderHeadDays () {
+      return days.value.map(day => __renderHeadDay(day))
+    }
+
+    function __renderHeadDaysRow () {
+      return h('div', {
+        class: {
+          'q-calendar-grid__head--days': true
+        }
+      }, [
+        ...__renderHeadDays()
+      ])
+    }
+
+    // ----
+
+    function __renderHead () {
+      return h('div', {
+        roll: 'presentation',
+        class: {
+          'q-calendar-grid__head': true,
+          'q-calendar__sticky': true
+        },
+        style: {
+        }
+      }, [
+        __renderHeadTask(),
+        __renderHeadDaysRow()
+      ])
+    }
+
+    function __renderContainer () {
+      return h('div', {
+        class: 'q-calendar-grid__container'
+      }, [
+        __renderScrollArea()
+      ])
+    }
+
+    function __renderScrollArea () {
+      return h('div', {
+        ref: scrollArea,
+        class: {
+          'q-calendar-grid__scroll-area': true,
+          'q-calendar__scroll': true
+        }
+      }, [
+        __renderBody()
+      ])
+    }
+
+    function __renderGrid () {
+      const { start, end } = renderValues.value
+      startDate.value = start.date
+      endDate.value = end.date
+
+      const hasWidth = size.width > 0
+
+      const weekly = withDirectives(h('div', {
+        key: startDate.value,
+        class: 'q-calendar-grid'
+      }, [
+        hasWidth === true && __renderContainer()
+      ]), [[
+        ResizeObserver,
+        __onResize
+      ]])
+
+      if (props.animated === true) {
+        const transition = 'q-calendar--' + (direction.value === 'prev' ? props.transitionPrev : props.transitionNext)
+        return h(Transition, {
+          name: transition,
+          appear: true
+        }, () => weekly)
+      }
+
+      return weekly
+    }
+
+    // expose public methods
+    expose({
+      prev,
+      next,
+      move,
+      moveToToday,
+      updateCurrent
+    })
+    // Object.assign(vm.proxy, {
+    //   prev,
+    //   next,
+    //   move,
+    //   moveToToday,
+    //   updateCurrent
+    // })
+
+    return () => __renderCalendar()
+  }
+
 })
---
+++ ui/src/composables/useGrid.js
@@ -0,0 +1,200 @@
+import {
+  computed
+  // watch
+} from 'vue'
+
+import {
+  DAYS_IN_WEEK,
+  addToDate,
+  copyTimestamp,
+  createDayList,
+  // createNativeLocaleFormatter,
+  // getDayIdentifier,
+  getEndOfWeek,
+  getStartOfWeek,
+  getEndOfMonth,
+  getStartOfMonth,
+  parseTimestamp,
+  today,
+  validateNumber,
+  validateTimestamp
+} from '../utils/Timestamp.js'
+
+export const useGridProps = {
+  modelValue: { // v-model
+    type: String,
+    default: today(),
+    validator: v => v === '' || validateTimestamp(v)
+  },
+  tasks: {
+    type: Array,
+    default: []
+  },
+  weekdays: {
+    type: Array,
+    default: () => [ 0, 1, 2, 3, 4, 5, 6 ]
+  },
+  view: {
+    type: String,
+    validator: v => [ 'day', 'week', 'month' ].includes(v),
+    default: 'month'
+  },
+  viewCount: {
+    type: Number,
+    default: 1,
+    validator: v => validateNumber(v) && v > 0
+  },
+  cellWidth: {
+    type: Object,
+    default: {
+      day: '50px',
+      week: '50px',
+      month: '50px'
+    }
+  },
+  minLabelLength: {
+    type: [ Number, String ],
+    default: 1
+  },
+  bordered: Boolean,
+  dark: Boolean,
+  disabledDays: Array,
+  disabledBefore: String,
+  disabledAfter: String,
+  disabledWeekdays: {
+    type: Array,
+    default: () => []
+  },
+  noHeader: Boolean,
+  locale: {
+    type: String,
+    default: 'en-us'
+  },
+  animated: Boolean,
+  transitionPrev: {
+    type: String,
+    default: 'slide-right'
+  },
+  transitionNext: {
+    type: String,
+    default: 'slide-left'
+  },
+  weekdayClass: Function,
+  dayClass: Function,
+  footerDayClass: Function,
+  dragEnterFunc: {
+    type: Function
+    // event, timestamp
+  },
+  dragOverFunc: {
+    type: Function
+    // event, timestamp
+  },
+  dragLeaveFunc: {
+    type: Function
+    // event, timestamp
+  },
+  dropFunc: {
+    type: Function
+    // event, timestamp
+  },
+  hoverable: Boolean,
+  focusable: Boolean,
+  focusType: {
+    type: Array,
+    default: ['date'],
+    validator: v => {
+      let val = true
+      v.forEach(type => {
+        if ([ 'day', 'date', 'weekday', 'resource' ].includes(type) !== true) {
+          val = false
+        }
+      })
+      return val
+    }
+  },
+  taskWidth: {
+    type: Number,
+    default: 200,
+    validator: v => validateNumber(v) && v > 0
+  }
+}
+
+export default function (props, emit, {
+  weekdaySkips,
+  times
+  // parsedStart,
+  // parsedEnd,
+  // size,
+  // headerColumnRef
+}) {
+  const parsedStartDate = computed(() => {
+    if (props.view === 'day') {
+      return parseTimestamp(props.modelValue)
+    }
+    else if (props.view === 'week') {
+      return getStartOfWeek(parseTimestamp(props.modelValue), props.weekdays, times.today)
+    }
+    else if (props.view === 'month') {
+      return getStartOfMonth(parseTimestamp(props.modelValue), props.weekdays, times.today)
+    }
+    else {
+      throw new Error(`QCalendarGrid: unknown 'view' type (${ props.view })`)
+    }
+  })
+
+  const parsedEndDate = computed(() => {
+    if (props.view === 'day') {
+      if (props.viewCount === 1) {
+        return parsedStartDate.value
+      }
+      let end = copyTimestamp(parsedStartDate.value)
+      end = addToDate(end, { day: props.viewCount - 1 })
+      return end
+    }
+    else if (props.view === 'week') {
+      if (props.viewCount === 1) {
+        return getEndOfWeek(parseTimestamp(props.modelValue), props.weekdays, times.today)
+      }
+      else {
+        let end = copyTimestamp(parsedStartDate.value)
+        end = addToDate(end, { day: (props.viewCount - 1) * DAYS_IN_WEEK })
+        return getEndOfWeek(end, props.weekdays, times.today)
+      }
+    }
+    else if (props.view === 'month') {
+      if (props.viewCount === 1) {
+        return getEndOfMonth(parseTimestamp(props.modelValue), props.weekdays, times.today)
+      }
+      else {
+        let end = copyTimestamp(parsedStartDate.value)
+        end = addToDate(end, { month: props.viewCount })
+        return getEndOfMonth(end, props.weekdays, times.today)
+      }
+    }
+    else {
+      throw new Error(`QCalendarGrid: unknown 'view' type (${ props.view })`)
+    }
+  })
+
+  const days = computed(() => {
+    return createDayList(
+      parsedStartDate.value,
+      parsedEndDate.value,
+      times.today,
+      weekdaySkips.value,
+      props.disabledBefore,
+      props.disabledAfter,
+      props.disabledWeekdays,
+      props.disabledDays,
+      Number.MAX_SAFE_INTEGER
+      // parsedMinDays.value
+    )
+  })
+
+  return {
+    days,
+    parsedStartDate,
+    parsedEndDate
+  }
+}
