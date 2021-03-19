9d53467edab88392e9d56b48ba0418d1f9d9f27f
Ferdinand Thiessen <rpm@fthiessen.de>
2021-03-19 11:38:23 +0100

    Register all components in install of index.js for app extension

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,5 +1,11 @@
 import { version } from '../package.json'
 import QCalendar from './components/QCalendar.js'
+import QCalendarAgenda from './components/QCalendarAgenda.js'
+import QCalendarDay from './components/QCalendarDay.js'
+import QCalendarMonth from './components/QCalendarMonth.js'
+import QCalendarResource from './components/QCalendarResource.js'
+import QCalendarScheduler from './components/QCalendarScheduler.js'
+import QCalendarTask from './components/QCalendarTask.js'
 
 import {
   PARSE_REGEX,
@@ -84,6 +90,12 @@ import {
 export {
   version,
   QCalendar,
+  QCalendarAgenda,
+  QCalendarDay,
+  QCalendarMonth,
+  QCalendarResource,
+  QCalendarScheduler,
+  QCalendarTask,
 
   // timestamp
   PARSE_REGEX,
@@ -165,86 +177,15 @@ export {
 
 export default {
   version,
-  QCalendar,
 
-  // timestamp
-  PARSE_REGEX,
-  PARSE_DATE,
-  PARSE_TIME,
-  DAYS_IN_MONTH,
-  DAYS_IN_MONTH_LEAP,
-  DAYS_IN_MONTH_MIN,
-  DAYS_IN_MONTH_MAX,
-  MONTH_MAX,
-  MONTH_MIN,
-  DAY_MIN,
-  DAYS_IN_WEEK,
-  MINUTES_IN_HOUR,
-  HOURS_IN_DAY,
-  FIRST_HOUR,
-  MILLISECONDS_IN_MINUTE,
-  MILLISECONDS_IN_HOUR,
-  MILLISECONDS_IN_DAY,
-  MILLISECONDS_IN_WEEK,
-  Timestamp,
-  TimeObject,
-  today,
-  getStartOfWeek,
-  getEndOfWeek,
-  getStartOfMonth,
-  getEndOfMonth,
-  parseTime,
-  validateTimestamp,
-  parsed,
-  parseTimestamp,
-  parseDate,
-  getDayIdentifier,
-  getTimeIdentifier,
-  getDayTimeIdentifier,
-  diffTimestamp,
-  updateRelative,
-  updateMinutes,
-  updateWeekday,
-  updateDayOfYear,
-  updateWorkWeek,
-  updateDisabled,
-  updateFormatted,
-  getDayOfYear,
-  getWorkWeek,
-  getWeekday,
-  isLeapYear,
-  daysInMonth,
-  copyTimestamp,
-  padNumber,
-  getDate,
-  getTime,
-  getDateTime,
-  nextDay,
-  prevDay,
-  relativeDays,
-  moveRelativeDays,
-  findWeekday,
-  getWeekdaySkips,
-  createDayList,
-  createIntervalList,
-  createNativeLocaleFormatter,
-  makeDate,
-  makeDateTime,
-  validateNumber,
-  isBetweenDates,
-  isOverlappingDates,
-  daysBetween,
-  weeksBetween,
-  addToDate,
-  compareTimestamps,
-  compareDate,
-  compareTime,
-  compareDateTime,
-  // helpers
-  convertToUnit,
-  indexOf,
-
-  install (Vue) {
-    Vue.component(QCalendar.name, QCalendar)
+  // Vue plugin
+  install (app, options) {
+    app.component(QCalendar.name, QCalendar)
+    app.component(QCalendarAgenda.name, QCalendarAgenda)
+    app.component(QCalendarDay.name, QCalendarDay)
+    app.component(QCalendarMonth.name, QCalendarMonth)
+    app.component(QCalendarResource.name, QCalendarResource)
+    app.component(QCalendarScheduler.name, QCalendarScheduler)
+    app.component(QCalendarTask.name, QCalendarTask)
   }
 }
