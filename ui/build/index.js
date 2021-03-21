679aac184f51fae46b4f759c402ac30b3a3afee2
Jeff Galbraith <jeff.galbraith@xendra.com>
2021-03-21 16:46:34 -0600

    fix(build): minor updates

--- ui/build/index.js
+++ ui/build/index.js
@@ -11,7 +11,8 @@ console.log()
 require('./script.app-ext.js').syncAppExt()
 require('./script.clean.js')
 
-console.log(` 📦 Building ${green('v' + require('../package.json').version)}...${parallel ? blue(' [multi-threaded]') : ''}\n`)
+// console.log(` 📦 Building ${green('v' + require('../package.json').version)}...${parallel ? blue(' [multi-threaded]') : ''}\n`)
+console.log(` 📦 Building ${green('v' + require('../package.json').version)}\n`)
 
 createFolder('dist')
 
