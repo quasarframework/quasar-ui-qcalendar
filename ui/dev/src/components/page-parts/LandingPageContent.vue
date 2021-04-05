f73b690a94e3f1477320d59920ceda3951d50605
Jeff Galbraith <galbraith64@gmail.com>
2021-04-05 14:38:59 -0600

    chore(dev): updates to landing page

--- ui/dev/src/components/page-parts/LandingPageContent.vue
+++ ui/dev/src/components/page-parts/LandingPageContent.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="q-px-lg q-pb-md">
-    <q-timeline :layout="layout" color="secondary">
+  <section class="flex flex-center bg-blue-10 text-white shadow-10">
+    <div class="landing-page__container">
+      <div class="landing-page__splash row justify-around items-center">
+        <h2 class="text-weight-bold text-blue-2 q-pa-sm" style="padding-left: 20px;">Build Beautiful,<br>Responsive<br> Calendars</h2>
+        <div class="q-pa-sm">
+          <img
+            src="/qcalendarmonth-event-slots.png"
+            class="shadow-10"
+            style="max-width: 340px;"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="full-width row justify-center q-pb-xl">
+      <q-btn
+        to="/all-about-qcalendar/what-is-qcalendar"
+        class="bg-red-8"
+      >
+        <div class="row justify-around items-center" style="width: 140px;">
+          <div>Get started</div>
+          <q-icon :name="biArrowRightCircle"/>
+        </div>
+      </q-btn>
+    </div>
+  </section>
+  <div class="q-px-lg q-pb-lg">
+    <q-timeline :layout="layout" color="primary">
 
       <q-timeline-entry heading>
         <div style="font-size: 32px; font-weight: 600;">QCalendar</div>
-        <div style="font-size: 16px;">Build Beautiful, Responsive Calendars</div>
+        <!-- <div style="font-size: 16px;">Build Beautiful,Responsive Calendars</div> -->
       </q-timeline-entry>
 
       <q-timeline-entry
@@ -58,7 +83,7 @@
             <q-img
               src="\QCalendarMonth--mini-mode.png"
               spinner-color="white"
-              style="max-width: 350px"
+              style="max-width: 200px"
               class="shadow-12"
             />
           </q-intersection>
@@ -189,7 +214,7 @@
 
       <q-timeline-entry
         title="Timesheets"
-        subtitle="Task Calendar has a footer for summary"
+        subtitle="Use a Footer summary with Task Calendar"
         side="left"
       >
         <div>
@@ -315,7 +340,7 @@
 
       <q-timeline-entry
         title="Dark"
-        subtitle="Automatic dark mode"
+        subtitle="Automatic dark mode when used with Quasar Framework"
         side="right"
         color="black"
       >
@@ -341,6 +366,12 @@
 <script>
 import { defineComponent, computed } from 'vue'
 import { useQuasar } from 'quasar'
+import {
+  // biTwitter,
+  // biGithub,
+  biArrowRightCircle
+
+} from '@quasar/extras/bootstrap-icons'
 
 export default defineComponent({
   name: 'Landing Page Content',
@@ -353,8 +384,36 @@ export default defineComponent({
     })
 
     return {
-      layout
+      layout,
+      biArrowRightCircle
     }
   }
 })
 </script>
+
+<style lang="sass" scoped>
+.landing-page-container
+  position: relative
+  margin: 0 auto
+  @media (max-width: $breakpoint-xs-max)
+    max-width: 340px
+    width: 340px
+  @media (min-width: $breakpoint-sm-min) and (max-width: $breakpoint-md-max)
+    max-width: 480px
+    width: 480px
+  @media (min-width: $breakpoint-lg-min)
+    max-width: 960px
+    width: 960px
+
+.landing-page__splash
+  @media (max-width: $breakpoint-xs-max)
+    padding: 0
+  @media (min-width: $breakpoint-sm-min) and (max-width: $breakpoint-md-max)
+    padding: 30px 0 60px
+  @media (min-width: $breakpoint-lg-min)
+    padding: 50px 0 100px
+
+.landing-page__splash--content
+  max-width: 500px
+
+</style>
