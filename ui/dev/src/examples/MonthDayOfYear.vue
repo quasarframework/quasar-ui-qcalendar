3c02c9186e18acf945b668f4093ba108a937f83d
Jeff Galbraith <galbraith64@gmail.com>
2021-04-12 16:47:46 -0600

    fix(docs/example): set 'day-min-height' property

--- ui/dev/src/examples/MonthDayOfYear.vue
+++ ui/dev/src/examples/MonthDayOfYear.vue
@@ -14,6 +14,7 @@
         animated
         bordered
         date-align="right"
+        day-min-height="50"
         @change="onChange"
         @moved="onMoved"
         @click-date="onClickDate"
