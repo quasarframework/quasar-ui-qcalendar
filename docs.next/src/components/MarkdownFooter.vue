50a5531531559e891b2150771be4367815d893c5
Jeff Galbraith <galbraith64@gmail.com>
2021-04-24 11:45:36 -0600

    chore(docs.next): add github profile link

--- docs.next/src/components/MarkdownFooter.vue
+++ docs.next/src/components/MarkdownFooter.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="markdown-page-footer__icons q-mt-md full-width row justify-center items-center q-gutter-sm">
-    <a href="https://github.com/hawkeye64" target="_blank" rel="noopener"><q-icon :name="fabGithub" /></a>
+    <a href="https://github.com/hawkeye64" target="_blank" rel="noopener"><q-avatar size="28px"><img src="~assets/profile.png"></q-avatar></a>
+    <a href="https://github.com/quasarframework/quasar-ui-qcalendar/tree/next" target="_blank" rel="noopener"><q-icon :name="fabGithub" /></a>
     <a href="https://twitter.com/jgalbraith64" target="_blank" rel="noopener"><q-icon :name="fabTwitter" /></a>
     <a href="https://github.com/sponsors/hawkeye64" target="_blank" rel="noopener"><q-icon :name="mdiCharity" /></a>
   </div>
-  <div class="full-width row justify-center items-center">
+  <div class="full-width row justify-center items-center q-my-sm">
     <p class="markdown-copyright">CC-BY / MIT License | Copyright &copy; 2018 - {{ year }} Jeff Galbraith</p>
   </div>
 </template>
 
 <script>
-// import { computed } from 'vue'
-// import { mdiLaunch } from '@quasar/extras/mdi-v5'
-
 import {
   fabGithub,
   fabTwitter
-  // fabFacebook
 } from '@quasar/extras/fontawesome-v5'
 
 import {
-  // mdiBlogger, mdiForum, mdiChat,
   mdiCharity
-  // mdiPencil, mdiLaunch,
-  // mdiChevronLeft, mdiChevronRight,
-  // mdiFlash
 } from '@quasar/extras/mdi-v5'
 
 const year = (new Date()).getFullYear()
 
 export default {
-  name: 'MarkdownLink',
+  name: 'MarkdownFooter',
 
   props: {
     to: String
