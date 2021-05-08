229de3e4dae4e8f27ff3321af5c429475c8be711
Jeff Galbraith <galbraith64@gmail.com>
2021-05-08 14:20:30 -0600

    feat(docs): Anatomy of a Calendar (add month) [wip]

---
+++ docs/src/components/page-parts/anatomy/QCalendarMonthAnatomy.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="row justify-center q-mt-md text-h5 text-bold">
+    QCalendarMonth
+  </div>
+  <div class="row full-width q-pa-xs rounded-borders" style="border: 2px solid rgba(25,118,210,.65);">
+    <q-scroll-area style="max-width: 180px; width: 100%; max-height: 300px; col">
+      <q-list dense>
+        <q-item
+          v-for="type in types"
+          :key="type.name"
+          clickable
+          :active="selected.name === type.name"
+          @click="updateSelection(type)"
+        >
+          <q-item-section>
+            {{ type.name }}
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-scroll-area>
+
+    <div class="col">
+      <q-calendar-month
+        id="calendar-month"
+        ref="calendar"
+        bordered
+        show-work-weeks
+        show-day-of-year-label
+        date-align="right"
+        style="height: 300px;"
+      >
+        <template #week />
+      </q-calendar-month>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, reactive, watch, onMounted } from 'vue'
+import { QCalendarMonth } from '@quasar/quasar-ui-qcalendar/src/QCalendarMonth.js'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarMonth.sass'
+
+export default defineComponent({
+  name: 'QCalendarMonthAnatomy',
+  components: {
+    QCalendarMonth
+  },
+  setup () {
+    const calendar = ref(null)
+    const types = reactive([
+        {
+          name: 'Head Workweek',
+          class: 'q-calendar-month__head--workweek'
+        },
+        {
+          name: 'Head',
+          class: 'q-calendar-month__head'
+        },
+        {
+          name: 'Workweek',
+          class: 'q-calendar-month__workweek'
+        },
+        {
+          name: 'Weekdays',
+          class: 'q-calendar-month__head--weekdays'
+        },
+        {
+          name: 'Weekday',
+          class: 'q-calendar-month__head--weekday'
+        },
+        {
+          name: 'Head Day Event',
+          class: 'q-calendar-month__head--event'
+        },
+        {
+          name: 'Week Row',
+          class: 'q-calendar-month__week--wrapper'
+        },
+        {
+          name: 'Week Days',
+          class: 'q-calendar-month__week--days'
+        },
+        {
+          name: 'Week Events',
+          class: 'q-calendar-month__week--events'
+        },
+        {
+          name: 'Day',
+          class: 'q-calendar-month__day'
+        },
+        {
+          name: 'Day Label Wrapper',
+          class: 'q-calendar-month__day--label__wrapper'
+        },
+        {
+          name: 'Day Label',
+          class: 'q-calendar-month__day--label'
+        },
+        {
+          name: 'Day of Year Label',
+          class: 'q-calendar-month__day--day-of-year'
+        },
+        {
+          name: 'Month Label',
+          class: 'q-calendar-month__day--month'
+        },
+        {
+          name: 'Day Content',
+          class: 'q-calendar-month__day--content'
+        },
+        {
+          name: 'Body',
+          class: 'q-calendar-month__body'
+        }
+      ])
+    const selected = ref(types[ 0 ])
+    const el = ref(null)
+
+    watch(() => selected.value, (current, prev) => {
+      removeClass(prev)
+      addClass(current)
+    })
+
+    onMounted(() => {
+      el.value = document.getElementById('calendar-month')
+      setTimeout(() => {
+        addClass(selected.value)
+      }, 350)
+    })
+
+    function updateSelection (type) {
+      selected.value = type
+    }
+
+    function addClass (type) {
+      if (type.class !== undefined) {
+        const els = el.value.querySelectorAll('.' + type.class)
+        if (els && els.length > 0) {
+          els.forEach(el => {
+            el.classList.add('highlight')
+          })
+        }
+      }
+    }
+
+    function removeClass (type) {
+      const els = el.value.querySelectorAll('.highlight')
+      if (els && els.length > 0) {
+        els.forEach(el => {
+          el.classList.remove('highlight')
+        })
+      }
+    }
+
+    return {
+      calendar,
+      selected,
+      types,
+      updateSelection
+    }
+  }
+})
+</script>
--- docs/src/pages/all-about-qcalendar/anatomy-of-a-calendar.md
+++ docs/src/pages/all-about-qcalendar/anatomy-of-a-calendar.md
@@ -5,7 +5,8 @@ keys: All about QCalendar
 components:
   - anatomy/QCalendarDayAnatomy
   - anatomy/QCalendarWeekAnatomy
+  - anatomy/QCalendarMonthAnatomy
 ---
 <q-calendar-day-anatomy />
-
 <q-calendar-week-anatomy />
+<q-calendar-month-anatomy />
