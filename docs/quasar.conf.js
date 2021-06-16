582269e585c611beb22e97c3cf11845858f97505
Tobias Mesquita <tobias.mesquita@gmail.com>
2021-06-16 09:47:57 -0300

    ssr (#307)
    Thanks @TobyMosque !

--- docs/quasar.conf.js
+++ docs/quasar.conf.js
@@ -155,7 +155,11 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false
+      pwa: false,
+      middlewares: [
+        ctx.prod ? 'compression' : '',
+        'render' // keep this as last one
+      ]
     },
 
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
