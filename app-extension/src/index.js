2e4242436ed93d67b3c581b2704196b67752c65a
Jeff Galbraith <galbraith64@gmail.com>
2021-06-23 07:17:21 -0600

    fix(app-ext): to be "compatibleWith" with quasar/v2 & app/v3

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -22,8 +22,8 @@ module.exports = function (api) {
   // Quasar compatibility check; you may need
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app" CLI
-  api.compatibleWith('quasar', '^2.0.0-beta.1')
-  api.compatibleWith('@quasar/app', '^3.0.0-beta.1')
+  api.compatibleWith('quasar', '^2.0.0')
+  api.compatibleWith('@quasar/app', '^3.0.0')
 
   // Uncomment the line below if you provide a JSON API for your component
   api.registerDescribeApi('QCalendar', '~@quasar/quasar-ui-qcalendar/dist/api/QCalendar.json')
