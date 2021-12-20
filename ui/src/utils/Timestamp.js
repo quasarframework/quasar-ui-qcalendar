98fcccb383bf78d934dcd8b0b70795118670bea4
Jeff Galbraith <galbraith64@gmail.com>
2021-12-20 08:53:49 -0700

    fix(timestamp): first day of week should be Sunday (to mimic browser)

--- ui/src/utils/Timestamp.js
+++ ui/src/utils/Timestamp.js
@@ -1132,15 +1132,15 @@ export function weeksBetween (ts1, ts2) {
   return Math.ceil(daysBetween(t1, t2) / DAYS_IN_WEEK)
 }
 
-// Known dates
+// Known dates - starting week on a monday to conform to browser
 const weekdayDateMap = {
+  Sun: new Date('2020-01-05T00:00:00.000Z'),
   Mon: new Date('2020-01-06T00:00:00.000Z'),
   Tue: new Date('2020-01-07T00:00:00.000Z'),
   Wed: new Date('2020-01-08T00:00:00.000Z'),
   Thu: new Date('2020-01-09T00:00:00.000Z'),
   Fri: new Date('2020-01-10T00:00:00.000Z'),
-  Sat: new Date('2020-01-11T00:00:00.000Z'),
-  Sun: new Date('2020-01-12T00:00:00.000Z'),
+  Sat: new Date('2020-01-11T00:00:00.000Z')
 }
 
 export function getWeekdayFormatter () {
