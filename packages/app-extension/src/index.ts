d11ce8386d37c748fd0642f7475ed3ed8aebf5db
Jeff <galbraith64@gmail.com>
2026-06-08 10:07:24 -0600

    chore: update app-vite beta.42

--- packages/app-extension/src/index.ts
+++ packages/app-extension/src/index.ts
@@ -13,7 +13,7 @@ export default defineIndexScript((api) => {
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app" CLI
   api.compatibleWith('quasar', '^2.16.0')
-  api.compatibleWith('@quasar/app-vite', '>=3.0.0-beta.41')
+  api.compatibleWith('@quasar/app-vite', '>=3.0.0-beta.42')
 
   // Uncomment the line below if you provide a JSON API for your component
   api.registerDescribeApi('QCalendar', '~@quasar/quasar-ui-qcalendar/dist/api/QCalendar.json')
